fc4a31b99e42937515919b6e92543cfef8c2d216
morrillo <gustavo.orrillo@moldeo.coop>
2016-10-14 13:25:56 -0300

    Added recommendation text

--- wizard/wizard_model.py
+++ wizard/wizard_model.py
@@ -13,9 +13,22 @@ _logger = logging.getLogger(__name__)
 class order_recommendation_lines(models.TransientModel):
 	_name = 'order.recommendation.line'
 
+	@api.one
+	def _compute_rule_explanation(self):
+		return_value = ''
+		if self.rule_recommended:
+			return_value = 'Cada vez que se compra el producto ' + self.product_id.name + '\n'
+			return_value = 'se compra tambien el/los productos '
+			for rule in self.rule_recommended.rhr.ids:
+				product_name = self.env['product.product'].browse(rule).name
+				return_value = return_value + product_name + ','
+			return_value = return_value + ' un ' + str(self.rule_recommended.confidence*100) + '% de veces'
+		self.rule_explanation = return_value
+
 	recommendation_id = fields.Many2one('order.recommendation')
 	product_id = fields.Many2one('product.product',string='Product')
 	rule_recommended = fields.Many2one('product.association.rule')
+	rule_explanation = fields.Char('Explicacion',compute=_compute_rule_explanation)
 
 
 class order_recommendation(models.TransientModel):
